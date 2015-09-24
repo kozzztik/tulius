a9352e21896b2cac8bf23a0afd23e0a32245c002
kozzztik <kozzztik@mail.ru>
2015-09-24 16:16:40 +0300

    TULIUS-656 Websockets for pm

--- wsgi_websocket.py
+++ wsgi_websocket.py
@@ -2,6 +2,6 @@ import os
 import gevent.socket
 import redis.connection
 redis.connection.socket = gevent.socket
-os.environ.update(DJANGO_SETTINGS_MODULE='my_app.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tulius.settings")
 from ws4redis.uwsgi_runserver import uWSGIWebsocketServer
 application = uWSGIWebsocketServer()
