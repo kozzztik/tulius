9561a717ece3e2a2443579f729839f8dcd4554a0
kozzztik <kozzztik@mail.ru>
2018-04-12 19:59:18 +0300

    Add on update deploy script.

--- scripts/on_update.sh
+++ scripts/on_update.sh
@@ -1,2 +1,13 @@
 echo "do on update"
-touch here.pid
+. env/bin/activate
+pip install -r requirements.txt
+export DJANGO_SETTINGS_MODULE=settings-production
+python manage.py collectstatic --noinput
+python manage.py syncdb
+python manage.py migrate
+python manage.py compilemessages
+deactivate
+chown -R travis:www-data *
+chmod 775 *
+sudo /etc/init.d/uwsgi restart
+
