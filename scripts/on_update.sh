52a9d5b516cf4f9ef0b5b9e2a099bf418474ad15
kozzztik <kozzztik@mail.ru>
2018-04-21 19:12:42 +0300

    Fix build

--- scripts/on_update.sh
+++ scripts/on_update.sh
@@ -10,9 +10,9 @@ echo "Stop existing tulius_$1"
 docker stop tulius_$1
 
 echo "Build docker container tulius_$1"
-docker build -f Dockerfile -t tulius_$1
+docker build -t tulius_$1 .
 
 echo "Start docker container $1 on port $2"
-docker run -d -p 7000:$2 --name=tulius_$1 -restart=unless-stopped \
+docker run -d -p 7000:$2 --name=tulius_$1 --restart=unless-stopped \
     -v "$PWD/media":/opt/tulius/media tulius_$1
 echo "Done."
