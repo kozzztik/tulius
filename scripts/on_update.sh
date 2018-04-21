e37b5187603f4aa92b7474a6a5388dbfe33e1410
kozzztik <kozzztik@mail.ru>
2018-04-22 00:47:35 +0300

    Fix build (websockets)

--- scripts/on_update.sh
+++ scripts/on_update.sh
@@ -3,6 +3,8 @@ echo "do on update"
 echo "Stop existing tulius_$1"
 docker stop tulius_$1
 docker rm tulius_$1
+docker stop tulius_$1_ws
+docker rm tulius_$1_ws
 
 echo "Build docker container tulius_$1"
 docker build -t tulius_$1 .
@@ -18,4 +20,15 @@ docker run tulius_$1 python manage.py migrate
 echo "Start docker container tulius_$1 on port $2"
 docker run -d -p 7000:$2 --name=tulius_$1 --restart=unless-stopped \
     -v "$PWD/media":/opt/tulius/media tulius_$1
+
+echo "Start docker container tulius_$1_ws on port $3"
+docker run -d -p 7000:$3 --name=tulius_$1_ws --restart=unless-stopped \
+    tulius_$1 uwsgi --socket 0.0.0.0:7000 --protocol uwsgi \
+               --max-requests 10000 \
+               --threads 2 \
+               --processes 1 \
+               --master \
+               --http-websockets \
+               --wsgi wsgi_websocket:application
+
 echo "Done."
