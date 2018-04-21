48aeb04fba2764141dab2babc4422219f7cee6d2
kozzztik <kozzztik@mail.ru>
2018-04-21 17:17:50 +0300

    Fix requirements.

--- scripts/on_update.sh
+++ scripts/on_update.sh
@@ -3,7 +3,6 @@ echo "do on update"
 pip install -r requirements.txt
 export DJANGO_SETTINGS_MODULE=settings-production
 python manage.py collectstatic --noinput
-python manage.py syncdb
 python manage.py migrate
 python manage.py compilemessages
 deactivate
