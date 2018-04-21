f07d9e3dfef81b36c0c5241b11b7d6b33aa51e30
kozzztik <kozzztik@mail.ru>
2018-04-21 19:57:48 +0300

    Fix build

--- scripts/on_update.sh
+++ scripts/on_update.sh
@@ -7,14 +7,12 @@ echo "Build docker container tulius_$1"
 docker build -t tulius_$1 .
 
 echo "Collect static"
-docker run -v "$PWD/static:/opt/tulius/static \
+docker run -v "$PWD/static":/opt/tulius/static \
     tulius_$1 python manage.py collectstatic --noinput
 
 echo "Migrate"
-docker run -v "$PWD/static:/opt/tulius/static \
-    tulius_$1 python manage.py syncdb
-docker run -v "$PWD/static:/opt/tulius/static \
-    tulius_$1 python manage.py migrate
+docker run tulius_$1 python manage.py syncdb
+docker run tulius_$1 python manage.py migrate
 
 
 echo "Start docker container tulius_$1 on port $2"
