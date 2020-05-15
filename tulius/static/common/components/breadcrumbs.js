c6818cc41e3367959802356ab4fa4e328e2654da
kozzztik <kozzztik@mail.ru>
2020-05-15 21:53:33 +0300

    Remove jQuery from breadcrumbs (pure CSS implementation)

--- tulius/static/common/components/breadcrumbs.js
+++ tulius/static/common/components/breadcrumbs.js
@@ -2,8 +2,5 @@ export default LazyComponent('breadcrumbs', {
     template: '/static/common/components/breadcrumbs.html',
     props: ['upper', 'items'],
     mounted() {
-        $(
-            this.upper ? "#top-breadcrumbs": "#bottom-breadcrumbs"
-        ).rcrumbs({nbFixedCrumbs: 1, nbUncollapsableCrumbs: 1});
     }
 })
