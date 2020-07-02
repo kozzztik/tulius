4da205a9605cce282ff2d2a1b6a0dd63ad4d4cd0
kozzztik <kozzztik@mail.ru>
2020-07-02 19:01:14 +0300

    Remove bunch of old staff

--- tulius/forum/threads/core.py
+++ tulius/forum/threads/core.py
@@ -1,7 +1,4 @@
 from django import dispatch
-from django import http
-from django import shortcuts
-from django.core import exceptions
 from django.utils.translation import ugettext_lazy as _
 
 from tulius.forum import plugins
@@ -11,25 +8,6 @@ ERROR_VALIDATION = _('there were some errors during form validation')
 
 
 class ThreadsCorePlugin(plugins.ForumPlugin):
-    def get_parent_thread(self, user, thread_id, is_room=None):
-        try:
-            thread_id = int(thread_id)
-        except:
-            raise http.Http404()
-        if is_room is None:
-            parent_post = shortcuts.get_object_or_404(
-                self.site.core.models.Thread,
-                id=thread_id, plugin_id=self.site_id)
-        else:
-            parent_post = shortcuts.get_object_or_404(
-                self.site.core.models.Thread,
-                id=thread_id, plugin_id=self.site_id, room=is_room)
-        if parent_post.check_deleted():
-            raise http.Http404(str(_('Post was deleted')))
-        if not parent_post.read_right(user):
-            raise exceptions.PermissionDenied()
-        return parent_post
-
     def room_descendants(self, user, room):
         if room.rght - room.lft <= 1:
             return [], []
@@ -77,70 +55,6 @@ class ThreadsCorePlugin(plugins.ForumPlugin):
             (thread.parent_id == room.id) or (thread.parent_id in room_ids)]
         return new_room_list, threads
 
-    def search_list(self, user, parent, **kwargs):
-        queryset = self.models.Thread.objects.filter(
-            plugin_id=self.site_id, deleted=False, parent=parent, **kwargs)
-        thread_list = []
-        for thread in queryset:
-            thread.parent = parent
-            if not thread.read_right(user):
-                continue
-            thread_list += [thread] + self.search_list(user, thread, **kwargs)
-        return thread_list
-
-    def expand_move_list(self, queryset, thread, user):
-        thread_list = []
-        for room in queryset:
-            if (
-                    not room.is_descendant_of(thread, include_self=True)) and \
-                    room.write_right(user):
-                thread_list += [room]
-                if room.get_descendant_count():
-                    subqueryset = room.get_children().filter(
-                        room=True, deleted=False)
-                    for subroom in subqueryset:
-                        subroom.parent = room
-                    thread_list += self.expand_move_list(
-                        subqueryset, thread, user)
-        return thread_list
-
-    def move_list(self, thread, user):
-        queryset = self.models.Thread.objects.filter(
-            plugin_id=self.site_id, level=0, room=True, deleted=False)
-        move_list = self.expand_move_list(queryset, thread, user)
-        if user.is_superuser and thread.room:
-            move_list = [None] + move_list
-        return move_list
-
-    def move_thread(self, thread, user, new_parent):
-        if not thread.edit_right(user):
-            raise http.Http404("no rights")
-        if new_parent:
-            if not new_parent.write_right(user):
-                raise http.Http404("no rights")
-        else:
-            if not user.is_superuser:
-                raise http.Http404("no rights")
-        if new_parent not in self.move_list(thread, user):
-            raise http.Http404("bad new parent")
-        if new_parent and new_parent.is_descendant_of(
-                thread, include_self=True):
-            raise http.Http404("can`t move to a descendant")
-        old_parent = thread.parent
-        thread.parent = new_parent
-        thread.save()
-        if old_parent and ((not new_parent) or (
-                old_parent.tree_id != new_parent.tree_id)):
-            obj = self.models.Thread.objects.get(
-                tree_id=old_parent.tree_id, parent=None)
-            self.repair_thread_counters(obj, only_stats=True)
-            obj.save()
-        if new_parent:
-            obj = self.models.Thread.objects.get(
-                tree_id=new_parent.tree_id, parent=None)
-            self.repair_thread_counters(obj, only_stats=True)
-            obj.save()
-
     def repair_thread_counters(self, thread=None, only_stats=False):
         if not only_stats:
             if thread:
@@ -159,21 +73,13 @@ class ThreadsCorePlugin(plugins.ForumPlugin):
             t.save()
 
     def init_core(self):
-        self.thread_view_signal = dispatch.Signal(
-            providing_args=["context", "user", "request"])
-
         self.thread_repair_counters = dispatch.Signal(providing_args=[])
         self.thread_on_create = dispatch.Signal(providing_args=['instance'])
         self.thread_on_update = dispatch.Signal(
             providing_args=["old_thread"])
-        self.core['get_parent_thread'] = self.get_parent_thread
         self.core['room_descendants'] = self.room_descendants
-        self.core['move_thread'] = self.move_thread
-        self.core['thread_move_list'] = self.move_list
         self.core['Thread_rebuild'] = self.repair_thread_counters
         self.core['rebuild_tree'] = self.repair_thread_counters
-        self.core['threads_search_list'] = self.search_list
-        self.signals['thread_view'] = self.thread_view_signal
         self.signals['thread_repair_counters'] = self.thread_repair_counters
         self.signals['thread_on_create'] = self.thread_on_create
         self.signals['thread_on_update'] = self.thread_on_update
--- tulius/gameforum/threads/__init__.py
+++ tulius/gameforum/threads/__init__.py
@@ -1,101 +1,48 @@
-from django.db.models.query_utils import Q
+from django.views import generic
 from django.conf.urls import url
 
-from tulius.forum.threads import views
 from tulius.forum.threads.plugin import ThreadsPlugin
-from tulius.stories.models import Role, AdditionalMaterial, \
-    Illustration
+
+
+class Index(generic.TemplateView):
+    template_name = 'base_vue.html'
 
 
 class GameThreadsPlugin(ThreadsPlugin):
-    # pylint: disable=too-many-branches
-    def get_parent_thread(self, user, thread_id, is_room=None):
-        thread = super(GameThreadsPlugin, self).get_parent_thread(
-            user, thread_id, is_room)
-        variation = thread.variation
-        thread.all_roles = Role.objects.filter(variation=variation)
-        thread.edit_marks = variation.game and variation.game.write_right(user)
-        if thread.game:
-            if thread.game.edit_right(user):
-                thread.roles_list = [
-                    role for role in thread.all_roles if not role.deleted]
-            elif thread.game.status >= 5:
-                thread.roles_list = [
-                    role for role in thread.all_roles
-                    if not role.deleted and role.show_in_character_list]
-            else:
-                thread.roles_list = [
-                    role for role in thread.all_roles
-                    if role.user_id == user.id]
-        else:
-            thread.roles_list = []
-        thread.character_list = [
-            role for role in thread.all_roles if not role.deleted]
-        query_materials = Q(variation=thread.variation) | Q(
-            story_id=thread.variation.story_id)
-        if thread.game and (not thread.game.edit_right(user)):
-            thread.character_list = [
-                role for role in thread.character_list
-                if role.show_in_character_list]
-            query_materials = query_materials & Q(admins_only=False)
-        thread.materials = AdditionalMaterial.objects.filter(query_materials)
-        thread.illustrations = Illustration.objects.filter(query_materials)
-        rolize_list = {}
-        for role in thread.all_roles:
-            rolize_list[role.id] = role
-        thread.rolize_list = rolize_list
-        return thread
-
-    def move_list(self, thread, user):
-        queryset = self.models.Thread.objects.filter(
-            plugin_id=self.site_id, level=0, tree_id=thread.tree_id)
-        return self.expand_move_list(queryset, thread, user)
-
     def get_urls(self):
         return [
-            url(r'^$', views.Index.as_view(), name='index'),
+            url(r'^$', Index.as_view(), name='index'),
             url(
                 r'^(?P<variation_id>\d+)/',
-                views.Index.as_view(), name='game_forum_variation'),
+                Index.as_view(), name='game_forum_variation'),
             url(
                 r'^(?P<variation_id>\d+)/room/(?P<pk>\d+)/',
-                views.Index.as_view(), name='room_new'),
+                Index.as_view(), name='room_new'),
             url(
                 r'^(?P<variation_id>\d+)/thread/(?P<pk>\d+)/',
-                views.Index.as_view(), name='thread_new'),
+                Index.as_view(), name='thread_new'),
             url(
                 r'^room/(?P<parent_id>\d+)/$',
-                views.Index.as_view(), name='room'),
+                Index.as_view(), name='room'),
             url(
                 r'^add_room/$',
-                views.Index.as_view(), name='add_room'),
+                Index.as_view(), name='add_room'),
             url(
                 r'^add_room/(?P<parent_id>\d+)/$',
-                views.Index.as_view(), name='add_room'),
+                Index.as_view(), name='add_room'),
             url(
                 r'^add_thread/(?P<parent_id>\d+)/$',
-                views.Index.as_view(), name='add_thread'),
+                Index.as_view(), name='add_thread'),
             url(
                 r'^edit_thread/(?P<thread_id>\d+)/$',
-                views.Index.as_view(), name='edit_thread'),
+                Index.as_view(), name='edit_thread'),
             url(
                 r'^thread/(?P<parent_id>\d+)/$',
-                views.Index.as_view(), name='thread'),
+                Index.as_view(), name='thread'),
             url(
                 r'^search/(?P<parent_id>\d+)/$',
-                views.Index.as_view(), name='search'),
+                Index.as_view(), name='search'),
             url(
                 r'^extended_search/(?P<pk>\d+)/$',
-                views.Index.as_view(), name='extended_search'),
-            url(
-                r'^thread/(?P<parent_id>\d+)/move/$',
-                views.MoveThreadSelect.as_view(plugin=self),
-                name='thread_move'),
-            url(
-                r'^thread/(?P<parent_id>\d+)/move/(?P<thread_id>\d+)/$',
-                views.MoveThreadConfirm.as_view(plugin=self),
-                name='thread_move_confirm'),
-            url(r'^thread/(?P<parent_id>\d+)/move/root/$',
-                views.MoveThreadConfirm.as_view(plugin=self),
-                name='thread_move_confirm'),
+                Index.as_view(), name='extended_search'),
         ]
