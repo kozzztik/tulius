5359b414bdb4ed6b1f5ed359bb3990a458c4dd87
kozzztik <kozzztik@mail.ru>
2020-08-30 17:31:20 +0300

    Fix jenkins script

--- scripts/jenkins.sh
+++ scripts/jenkins.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
-
-export BRANCH_NAME=$(echo "$1" | sed 's/origin\///')
+GIT_BRANCH=$1
+export BRANCH_NAME=$(echo "$GIT_BRANCH" | sed 's/origin\///')
 docker build -t kozzztik/tulius:${BRANCH_NAME} .
 
 docker run -e TULIUS_BRANCH=test -e COV_BRANCH="$BRANCH_NAME" -e DJANGO_SETTINGS_MODULE="settings_production" --net="tuliusnet" \
@@ -12,5 +12,5 @@ kozzztik/tulius:${BRANCH_NAME} /opt/tulius/travis_test.sh
 if [[ $BRANCH_NAME = dev ]] || [[ $BRANCH_NAME = master ]]
 then
     ssh -i /mnt/big/jenkins/travis_rsa travis@$BRANCH_NAME.tulius.co-de.org -p 22 "cd ~/$BRANCH_NAME && git fetch --all && git reset --hard && git pull --rebase"
-	ssh -i /mnt/big/jenkins/travis_rsa travis@$BRANCH_NAME.tulius.co-de.org -p 22 "cd ~/$BRANCH_NAME && . scripts/on_update.sh $BRANCH_NAME"
+	  ssh -i /mnt/big/jenkins/travis_rsa travis@$BRANCH_NAME.tulius.co-de.org -p 22 "cd ~/$BRANCH_NAME && . scripts/on_update.sh $BRANCH_NAME"
 fi
