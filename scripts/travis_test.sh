c02aad8f5e7c174a00363b97df3495ca34734844
kozzztik <kozzztik@mail.ru>
2020-09-07 15:22:31 +0300

    Add some deploy fixes

--- scripts/travis_test.sh
+++ scripts/travis_test.sh
@@ -4,4 +4,5 @@ echo "Running pylint"
 python3 -m pylint djfw tulius tests
 python3 -m pytest tests --cov=tulius --cov-report term-missing --cov-report term:skip-covered
 TRAVIS_BRANCH=$COV_BRANCH
+echo "$TRAVIS_BRANCH"
 coveralls
