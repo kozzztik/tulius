239f812b6dfbfdbcf7d58864ab36f7b7f5c94a08
kozzztik <kozzztik@mail.ru>
2020-09-07 15:41:38 +0300

    Fix dev mounting folders

--- scripts/travis_test.sh
+++ scripts/travis_test.sh
@@ -3,6 +3,6 @@ set -ex
 echo "Running pylint"
 python3 -m pylint djfw tulius tests
 python3 -m pytest tests --cov=tulius --cov-report term-missing --cov-report term:skip-covered
-TRAVIS_BRANCH=$COV_BRANCH
+TRAVIS_BRANCH=$GIT_BRANCH
 echo "$TRAVIS_BRANCH"
 coveralls
