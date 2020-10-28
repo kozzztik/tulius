1d926eb734ac871ef7230142b2d691dbd9b43547
kozzztik <kozzztik@mail.ru>
2020-10-28 12:19:51 +0300

    Squash game migrations

--- tulius/stories/migrations/0001_squashed_0002_auto_20200724_2142.py
+++ tulius/stories/migrations/0001_squashed_0002_auto_20200724_2142.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('game_forum_threads', '0001_squashed_0006_auto_20201013_1740'),
-        ('games', '0001_initial'),
+        ('games', '0001_squashed_0002_auto_20180418_1722'),
     ]
 
     operations = [
