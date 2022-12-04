c4cd33282fa629560a37d7844cb9962dce718fe8
Konstantin Volkov <kozzztik@mail.ru>
2022-12-04 19:10:35 +0300

    Support of deploy different branches on same environment (#310)
    * Support of deploy different branches on same environment

--- scripts/on_update.sh
+++ scripts/on_update.sh
@@ -5,8 +5,14 @@ echo "do on update"
 TULIUS_BRANCH=$1
 ROOTDIR=$PWD
 
+if [ $2 ]; then
+  ENV = $2
+  echo "Deploy $TULIUS_BRANCH on $ENV environment"
+else
+  ENV = TULIUS_BRANCH
+fi
 echo "Stop existing compose"
-cd scripts/tulius/$1
+cd scripts/tulius/$ENV
 docker-compose stop
 docker-compose exec celery python manage.py wait_celery
 docker-compose down
@@ -20,22 +26,22 @@ docker pull kozzztik/tulius:$1
 
 echo "Collect static"
 docker run -v "$PWD/data/static":/opt/tulius/data/static \
-    -e TULIUS_BRANCH="$1" \
+    -e TULIUS_BRANCH="$ENV" \
     --net tuliusnet \
-    -v "/home/travis/$1/settings_production.py":/opt/tulius/settings_production.py \
+    -v "/home/travis/$ENV/settings_production.py":/opt/tulius/settings_production.py \
     kozzztik/tulius:$1 python manage.py collectstatic --noinput
 
 echo "Migrate"
 docker run \
-    -e TULIUS_BRANCH="$1" \
+    -e TULIUS_BRANCH="$ENV" \
     --net tuliusnet \
-    -v "/home/travis/$1/settings_production.py":/opt/tulius/settings_production.py \
+    -v "/home/travis/$ENV/settings_production.py":/opt/tulius/settings_production.py \
     kozzztik/tulius:$1 python manage.py migrate
 
 
 echo "Run compose"
 
-cd scripts/tulius/$1
+cd scripts/tulius/$ENV
 docker-compose up -d --build --force-recreate
 cd $ROOTDIR
 
