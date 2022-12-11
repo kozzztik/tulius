64cbd6e6f4f1d4d576743831efaf90c994f96349
kozzztik <kozzztik@mail.ru>
2022-12-11 18:32:16 +0300

    Better gitlab integration

--- scripts/gitlab_build.sh
+++ scripts/gitlab_build.sh
@@ -5,10 +5,12 @@ COMMIT=$2
 echo $COMMIT
 BRANCH_NAME=$(echo "$REF" | sed 's/origin\///' | sed 's/refs\/heads\///')
 echo $BRANCH_NAME
+export GITHUB="curl -X POST -H \"Accept: application/vnd.github+json\" -H \"Authorization: Bearer $GITHUB_KEY\" https://api.github.com/repos/kozzztik/tulius/statuses/$COMMIT"
+
+# export env variables for next stages
 echo "BRANCH_NAME=$BRANCH_NAME" >> build.env
 echo "REF=$REF" >> build.env
 echo "COMMIT=$COMMIT" >> build.env
-export GITHUB="curl -X POST -H \"Accept: application/vnd.github+json\" -H \"Authorization: Bearer $GITHUB_KEY\" https://api.github.com/repos/kozzztik/tulius/statuses/$COMMIT"
 echo "GITHUB=$GITHUB" >> build.env
 eval "$GITHUB -d '{\"state\":\"pending\",\"target_url\":\"$CI_PIPELINE_URL\",\"description\":\"Build in progress\",\"context\":\"CI/GitLab\"}'"
 docker build -t kozzztik/tulius:${BRANCH_NAME} .
