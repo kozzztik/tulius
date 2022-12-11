976175bfcc97e180d52ec6878f2df9e60775890a
kozzztik <kozzztik@mail.ru>
2022-12-11 19:35:55 +0300

    Better gitlab integration

--- scripts/gitlab_build.sh
+++ scripts/gitlab_build.sh
@@ -2,9 +2,7 @@
 set -ex
 REF=$1
 COMMIT=$2
-echo $COMMIT
 BRANCH_NAME=$(echo "$REF" | sed 's/origin\///' | sed 's/refs\/heads\///')
-echo $BRANCH_NAME
 export GITHUB="curl -X POST -H \"Accept: application/vnd.github+json\" -H \"Authorization: Bearer $GITHUB_KEY\" https://api.github.com/repos/kozzztik/tulius/statuses/$COMMIT"
 
 # export env variables for next stages
