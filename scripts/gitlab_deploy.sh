2b2f7cf548e7dbb95af2963040845676f6e1d8d8
kozzztik <kozzztik@mail.ru>
2022-12-11 19:47:25 +0300

    Better gitlab integration

--- scripts/gitlab_deploy.sh
+++ scripts/gitlab_deploy.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 set -ex
-ENV=$1
-BRANCH=$2
-COMMIT=$3
+KEY=$1
+ENV=$2
+BRANCH=$3
+COMMIT=$4
 echo "Push to repo"
 docker push kozzztik/tulius:$BRANCH
 
 # Download updates
-ssh -i /tmp/deploy_rsa travis@$ENV.tulius.co-de.org -p 22 "cd ~/$ENV && git fetch --all && git reset --hard && git git checkout -b $BRANCH $COMMIT"
+ssh -i $1 travis@$ENV.tulius.co-de.org -p 22 "cd ~/$ENV && git fetch --all && git reset --hard && git git checkout -b $BRANCH $COMMIT"
 echo "Data updated"
 
 # Do after deploy staff
-ssh -i /tmp/deploy_rsa travis@$ENV.tulius.co-de.org -p 22 "cd ~/$ENV && . scripts/on_update.sh $BRANCH $ENV"
+ssh -i $1 travis@$ENV.tulius.co-de.org -p 22 "cd ~/$ENV && . scripts/on_update.sh $BRANCH $ENV"
 echo "Finished."
 eval "$GITHUB -d '{\"state\":\"success\",\"target_url\":\"$CI_JOB_URL\",\"description\":\"Deployed on $ENV\",\"context\":\"Deploy/Production\"}'"
