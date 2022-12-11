e2c5f379ee75ac1e749df069590b8b44d654e5b3
Konstantin Volkov <kozzztik@mail.ru>
2022-12-11 21:10:02 +0300

    Fix deploy networking (#325)

--- scripts/on_update.sh
+++ scripts/on_update.sh
@@ -16,7 +16,7 @@ cd scripts/tulius/$ENV
 # First stop web then wait till celery will finish all tasks
 docker-compose stop uwsgi
 docker-compose exec celery python manage.py wait_celery || true
-docker-compose down
+docker-compose down --remove-orphans
 docker system prune --force
 docker rm -v $(docker ps --filter status=exited -q 2>/dev/null) 2>/dev/null
 docker rmi $(docker images --filter dangling=true -q 2>/dev/null) 2>/dev/null
