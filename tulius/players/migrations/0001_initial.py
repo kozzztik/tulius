dbe6cb22eb057dc54ea81b1e597b140651d603e3
kozzztik <kozzztik@mail.ru>
2023-08-13 19:46:36 +0300

    Linter fixes

--- tulius/players/migrations/0001_initial.py
+++ tulius/players/migrations/0001_initial.py
@@ -14,7 +14,9 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='PlayerStar',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(
+                    auto_created=True, primary_key=True, serialize=False,
+                    verbose_name='ID')),
                 ('games', models.SmallIntegerField(verbose_name='Games won')),
             ],
             options={
