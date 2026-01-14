79d480ef456c3511b720243a7d51cef5d38ab9ca
kozzztik <kozzztik@mail.ru>
2026-01-14 23:59:36 +0300

    fix coveralls

--- scripts/travis_test.sh
+++ scripts/travis_test.sh
@@ -3,4 +3,4 @@ set -ex
 echo "Running pylint"
 python3 -m pylint djfw tulius tests
 python3 -m pytest djfw tulius tests --cov=tulius --cov-report term-missing --cov-report term:skip-covered
-coveralls
+# coveralls
